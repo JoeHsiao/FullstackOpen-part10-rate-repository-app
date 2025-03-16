7bdc1efeb014f8b2ff7e2d18dc56b2eae10c9e97
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-15 21:25:53 -0700

    complete 10.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const compat = new FlatCompat({
 });
 
 export default defineConfig([{
+    files: ["**/*.js", "**/*.jsx"],
     extends: compat.extends("eslint:recommended", "plugin:react/recommended"),
 
     plugins: {
@@ -26,6 +27,7 @@ export default defineConfig([{
     languageOptions: {
         globals: {
             ...reactNative.environments["react-native"]["react-native"],
+            console: "readonly",
         },
 
         parser: babelParser,
